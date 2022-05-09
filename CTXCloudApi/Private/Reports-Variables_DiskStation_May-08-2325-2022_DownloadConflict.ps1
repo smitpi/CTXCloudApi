0f85aee6c88cd75335b0881790159b038a5911de
Pierre Smit <pierre.smit@outlook.com>
2022-05-09 09:53:55 +0200

    Create Reports-Variables_DiskStation_May-08-2325-2022_DownloadConflict.ps1

---
+++ CTXCloudApi/Private/Reports-Variables_DiskStation_May-08-2325-2022_DownloadConflict.ps1
@@ -0,0 +1,60 @@
+﻿
+
+$script:RegistrationState = [PSCustomObject]@{
+    0 = 'Unknown'
+    1 = 'Registered'
+    2 = 'Unregistered'
+}
+$script:ConnectionState = [PSCustomObject]@{
+    0 = 'Unknown'
+    1 = 'Connected'
+    2 = 'Disconnected'
+    3 = 'Terminated'
+    4 = 'PreparingSession'
+    5 = 'Active'
+    6 = 'Reconnecting'
+    7 = 'NonBrokeredSession'
+    8 = 'Other'
+    9 = 'Pending'
+}
+$script:ConnectionFailureType = [PSCustomObject]@{
+    0 = 'None'
+    1 = 'ClientConnectionFailure'
+    2 = 'MachineFailure'
+    3 = 'NoCapacityAvailable'
+    4 = 'NoLicensesAvailable'
+    5 = 'Configuration'
+}
+$script:SessionFailureCode = [PSCustomObject]@{
+    0   = 'Unknown'
+    1   = 'None'
+    2   = 'SessionPreparation'
+    3   = 'RegistrationTimeout'
+    4   = 'ConnectionTimeout'
+    5   = 'Licensing'
+    6   = 'Ticketing'
+    7   = 'Other'
+    8   = 'GeneralFail'
+    9   = 'MaintenanceMode'
+    10  = 'ApplicationDisabled'
+    11  = 'LicenseFeatureRefused'
+    12  = 'NoDesktopAvailable'
+    13  = 'SessionLimitReached'
+    14  = 'DisallowedProtocol'
+    15  = 'ResourceUnavailable'
+    16  = 'ActiveSessionReconnectDisabled'
+    17  = 'NoSessionToReconnect'
+    18  = 'SpinUpFailed'
+    19  = 'Refused'
+    20  = 'ConfigurationSetFailure'
+    21  = 'MaxTotalInstancesExceeded'
+    22  = 'MaxPerUserInstancesExceeded'
+    23  = 'CommunicationError'
+    24  = 'MaxPerMachineInstancesExceeded'
+    25  = 'MaxPerEntitlementInstancesExceeded'
+    100 = 'NoMachineAvailable'
+    101 = 'MachineNotFunctional'
+}
+
+
+
