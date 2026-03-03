0689fdc2ccdf873d58c2b64b496d922e8729e9aa
Pierre Smit <pierre.smit@outlook.com>
2026-03-03 17:03:17 +0100

    Update tests.ps1

--- Removed/tests.ps1
+++ Removed/tests.ps1
@@ -2,24 +2,27 @@
 
 
 Import-Module "C:\Users\ladmin\Dropbox\#Profile\Documents\PowerShell\ProdModules\CTXCloudApi\CTXCloudApi\CTXCloudApi.psm1" -Force -Verbose
+$SecureSecret = Import-Clixml -Path "C:\Secure\CTX_Secret.xml"
 
 $api = Connect-CTXAPI -Customer_Id $PrdCustomerID -Client_Id $Client_Id -Client_Secret $Client_Secret -Customer_Name Rabobankprd
 $mon = Get-CTXAPI_MonitorData -APIHeader $api -LastHours 6 -MonitorDetails All -verbose
 $app = Get-CTXAPI_Application -APIHeader $api
-$connection = Get-CTXAPI_ConnectionReport -APIHeader $api -MonitorData $mon
 $dl = Get-CTXAPI_DeliveryGroup -APIHeader $api -verbose
-$connection = Get-CTXAPI_FailureReport -APIHeader $api -MonitorData $mon -FailureType Connection
-$machinefail = Get-CTXAPI_FailureReport -APIHeader $api -MonitorData $mon -FailureType Machine
 $hyp = Get-CTXAPI_Hypervisor -APIHeader $api
 $mac = Get-CTXAPI_Machine -APIHeader $api
+Get-CTXAPI_Machine -APIHeader $api -Name 1LABCTXPRD7355
 $cat = Get-CTXAPI_MachineCatalog -APIHeader $api
-$util = Get-CTXAPI_ResourceUtilization -APIHeader $api -MonitorData $mon -Verbose
 $session = Get-CTXAPI_Session -APIHeader $api
 $site = Get-CTXAPI_SiteDetail -APIHeader $api
-$uptime = Get-CTXAPI_VDAUptime -APIHeader $api -Verbose
 $zone = Get-CTXAPI_Zone -APIHeader $api
-Test-CTXAPI_Header -APIHeader $api -AutoRenew
+
 $action = Set-CTXAPI_MachinePowerState -APIHeader $api -Name 1LABCTXPRD7355 -Action Shutdown
+Set-CTXAPI_MachineMaintenanceMode -APIHeader $api -Name 1LABCTXPRD7355 -InMaintenanceMode $true 
+
+Set-CTXAPI_MachineMaintenanceMode -APIHeader $api -Name 1LABCTXPRD7355 -InMaintenanceMode $false | Get-CTXAPI_Machine -APIHeader $api
+
+New-CTXAPI_Report
+
 
 Get-CTXAPI_CloudService -APIHeader $api
 Get-CTXAPI_LowLevelOperation -APIHeader $api
